15f9089f4290d54e067da72128c7a581b6cfdd3e
Hyeri Jang <chjang5621@naver.com>
2024-12-11 23:20:34 +0900

    로거 만들기

---
+++ src/thread/start/MyLoggerMain.java
@@ -0,0 +1,10 @@
+package thread.start;
+
+import static util.MyLogger.log;
+
+public class MyLoggerMain {
+    public static void main(String[] args) {
+        log("Hello, World!");
+        log(123);
+    }
+}
