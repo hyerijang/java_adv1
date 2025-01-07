50a55f6b35a28385704fb8b41cb57ddd53b33c2b
Hyeri Jang <chjang5621@naver.com>
2025-01-07 18:34:40 +0900

    LockSupport 1

--- src/thread/sync/lock/LockSupportMainV1.java
+++ src/thread/sync/lock/LockSupportMainV1.java
@@ -1,7 +1,6 @@
 package thread.sync.lock;
 
 import java.util.concurrent.locks.LockSupport;
-import static java.util.concurrent.locks.LockSupport.unpark;
 import static util.MyLogger.log;
 import static util.ThreadUtils.sleep;
 
@@ -15,8 +14,13 @@ public class LockSupportMainV1 {
         sleep(1000);
         log("Thread-1 state : " + thread1.getState());
         log("main -> Thread-1  unpark");
-        // 스레드 1 깨운다.
-        unpark(thread1);
+
+        // NOTE : WAITING인 스레드 1을 깨우는 방법
+        // (1) unpark() 호출
+        // (2)  interrupt() 호출
+//        unpark(thread1);
+        thread1.interrupt();
+        log("main 종료");
     }
 
     static class ParkTest implements Runnable {
@@ -24,7 +28,7 @@ public class LockSupportMainV1 {
         @Override
         public void run() {
             log("park 시작");
-            LockSupport.park();
+            LockSupport.park(); // NOTE : 현재 스레드를 WAITING 상태로 만듬
             log("park 종료, state :" + Thread.currentThread().getState());
             log("인터럽트 상태 : " + Thread.currentThread().isInterrupted());
         }
