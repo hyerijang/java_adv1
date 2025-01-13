08b23e9d96e41685604dd0b780c6c1019568a65d
Hyeri Jang <chjang5621@naver.com>
2025-01-13 18:12:54 +0900

    생산자 소비자 문제 V6_1 - BlockingQueue

---
+++ src/thread/bounded/BoundedQueueV6_1.java
@@ -0,0 +1,36 @@
+package thread.bounded;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class BoundedQueueV6_1 implements BoundedQueue {
+
+    private BlockingQueue<String> queue;
+
+    public BoundedQueueV6_1(int max) {
+        this.queue = new ArrayBlockingQueue<>(max);
+    }
+
+    @Override
+    public void put(String data) {
+        try {
+            queue.put(data);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public String take() {
+        try {
+            return queue.take();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public String toString() { // NOTE : synchronized 를 사용하지 않음 (원칙적으로는 사용하는게 맞지만, 예제에서는 데이터 확인이 크게 중요하지 않음 + 예제에서는 꼬일 수 있기 때문에 생략)
+        return queue.toString();
+    }
+}
