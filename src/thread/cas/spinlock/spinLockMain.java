148841883b32d34fb6858537111fc7fc84a423e7
Hyeri Jang <chjang5621@naver.com>
2025-01-13 21:52:20 +0900

    CAS로 스핀락 구현1 - 문제 있는 코드

---
+++ src/thread/cas/spinlock/spinLockMain.java
@@ -0,0 +1,24 @@
+package thread.cas.spinlock;
+
+import static util.MyLogger.log;
+
+public class spinLockMain {
+
+    public static void main(String[] args) {
+        SpinLockBad spinLock = new SpinLockBad();
+
+        Runnable task = () -> {
+            spinLock.lock();
+            try {
+                log("비즈니스 로직 실행");
+            } finally {
+                spinLock.unlock();
+            }
+        };
+
+        Thread thread1 = new Thread(task, "Thread-1");
+        Thread thread2 = new Thread(task, "Thread-2");
+        thread1.start();
+        thread2.start();
+    }
+}
