3794efd0c9fff4a13049a04053edb152bf38e37d
Hyeri Jang <chjang5621@naver.com>
2025-01-21 17:19:56 +0900

    [섹션 13] Executor 프레임워크 소개

---
+++ src/thread/executor/ExecutorUtils.java
@@ -0,0 +1,22 @@
+package thread.executor;
+
+import static util.MyLogger.log;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class ExecutorUtils {
+
+    public static void printState(ExecutorService executorService) {
+        if (executorService instanceof ThreadPoolExecutor poolExecutor) {
+            int pool = poolExecutor.getPoolSize();
+            int active = poolExecutor.getActiveCount();
+            int queued = poolExecutor.getQueue().size();
+            long completedTaskCount = poolExecutor.getCompletedTaskCount();
+            log("[pool=" + pool + ", activeCount=" + active + ", queuedCount=" + queued + ", completedTaskCount=" +
+                completedTaskCount + "]");
+        }
+        else {
+            log("ExecutorService is not ThreadPoolExecutor\n" + executorService);
+        }
+    }
+}
