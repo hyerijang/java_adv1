b6b8777a42711f191528313d65cf8533160b207d
Hyeri Jang <chjang5621@naver.com>
2025-02-06 17:12:30 +0900

    Future6 - 취소

---
+++ src/thread/executor/FutureCancleMain.java
@@ -0,0 +1,63 @@
+package thread.executor;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class FutureCancleMain {
+
+//    private static boolean mayInterruptIfRunning = true;
+    private static boolean mayInterruptIfRunning = false;
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        ExecutorService es = Executors.newFixedThreadPool(1);
+        Future<String> future = es.submit(new MyTask());
+        log("future.state = " + future.state());
+
+        //일정 시간 후 취소 시도
+        sleep(3000);
+
+        //cancle() 호출
+        log("future.cancel (" + mayInterruptIfRunning + ") 호출");
+        boolean cancelResult = future.cancel(mayInterruptIfRunning);
+        log("cancel (" + mayInterruptIfRunning + ")  Result = " + cancelResult);
+
+        // 결과 확인
+        try {
+            log("future.get() = " + future.get());
+        }
+        catch (CancellationException e) { // 런타임 예외
+            log("Future가 이미 취소되었습니다.");
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        // Excutor 종료
+        es.close();
+        
+    }
+
+    static class MyTask implements Callable<String> {
+        @Override
+        public String call() {
+            try {
+                for (int i = 0; i < 10; i++) {
+                    log("작업 중..." + i);
+                    Thread.sleep(1000);
+                }
+            } catch (InterruptedException e) {
+                log("인터럽트 발생");
+                return "Interrupted";
+            }
+
+            return "Completed";
+        }
+    }
+
+}
